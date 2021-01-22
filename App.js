5685afb891b3c8e74247391694ef5d4e21fbca57
Nayent <guilhermenomura1999@gmail.com>
2021-01-22 00:47:47 -0300

    a

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'Components',
     defaultNavigationOptions: {
-      title: 'First APP',
+      title: 'First asAPP',
     },
   }
 );
