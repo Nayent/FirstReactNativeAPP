8e600a1fdeec55c43b66b93be36f33dc9994c61c
Nayent <guilhermenomura1999@gmail.com>
2021-02-02 13:38:55 -0300

    Starting again

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+import ImageScreen from './src/screens/ImageScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
   },
   {
-    initialRouteName: 'Components',
+    initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: 'First APP',
+      title: 'App',
     },
   }
 );
